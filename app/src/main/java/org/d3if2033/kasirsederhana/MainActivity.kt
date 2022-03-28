d7afbe746623b804fafc78dbb019734d6a2ef300
Raihan Khoerul Imam <raihanhori101@gmail.com>
2022-03-28 15:53:52 +0700

    membuat ui desain

---
+++ app/src/main/java/org/d3if2033/kasirsederhana/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3if2033.kasirsederhana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
